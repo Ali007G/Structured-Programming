599f035b6db696a7df350cb448c823e45f1816cb
Ali <95523475+Ali007G@users.noreply.github.com>
2023-10-15 18:48:09 +0600

    Add files via upload

---
+++ C Programming Beginner/69. Fibonacci using array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[20];
+    printf("Enter number: ");
+    scanf("%d", &n);
+    a[0]=0;
+    a[1]=1;
+
+      for(i=2;i<n;i++)
+        a[i]=a[i-1]+a[i-2];
+   {
+       for(i=0;i<n;i++)
+          printf("%d\t", a[i]);   /// printing the value given by user input(0-5)
+   }
+}
