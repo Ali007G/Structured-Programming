617f1e5fde521f3000601e6a8e4fe3b0651647d1
Ali <95523475+Ali007G@users.noreply.github.com>
2023-10-15 18:49:33 +0600

    Add files via upload

---
+++ C Programming Beginner/72. Bubble Sort Array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+      int a[20];
+      int b,i,j,n;
+      printf("Enter number: ");
+      scanf("%d", &n);
+      printf("Enter array elements:\n");
+        for(i=0;i<n;i++)
+    {
+        printf("Enter element %d:", i+1);
+        scanf("%d", &a[i]);
+    }
+        for(i=0;i<n-1;i++)
+        {
+            for(j=0;j<n-1-i;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    b=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=b;
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+            printf("a[%d]=%d ", i,a[i]);
+}
