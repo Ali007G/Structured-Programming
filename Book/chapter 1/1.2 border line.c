234d92251e5fb4dce125a25e3ce7a51fb3da4052
Ali <95523475+Ali007G@users.noreply.github.com>
2023-10-18 20:21:37 +0600

    Add files via upload

---
+++ Book/chapter 1/1.2 border line.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("..............................................\n");
+    printf(".    Name           :  Golam Ali             .\n");
+    printf(".    Door No,street : 104,3                  .\n");
+    printf(".    City,Pin       : Cumilla, 3500          .\n");
+    printf("..............................................\n");
+
+    return 0;
+
+}
