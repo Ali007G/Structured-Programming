f1e91ca5a0f39d2f3fb62a2a7ff4f572b2f3ac3b
Ali <95523475+Ali007G@users.noreply.github.com>
2023-10-19 20:11:17 +0600

    Add files via upload

---
+++ Book/chapter 1/1.15 simple arithmetic calculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+                    printf("..............\n");
+     printf("X=  ...\n");
+                    printf("..............\n");
+
+     printf("sum=  ...\n");
+                    printf(".....\n");
+
+                    return 0;
+
+}
