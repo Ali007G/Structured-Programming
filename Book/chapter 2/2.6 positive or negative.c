483764ccc7c310fe406c2213cd699ff405565d1b
Ali <95523475+Ali007G@users.noreply.github.com>
2023-10-20 19:59:07 +0600

    Add files via upload

---
+++ Book/chapter 2/2.6 positive or negative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    if(num>0)
+    {
+    printf("%d positive", num);
+    }
+    else
+    {
+    printf("%d negative", num);
+    }
+
+    return 0;
+}
