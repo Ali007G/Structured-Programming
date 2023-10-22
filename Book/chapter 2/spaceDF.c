6307ce29453edbaeac5afa0606b3811ad3cf06a3
Ali <95523475+Ali007G@users.noreply.github.com>
2023-10-22 19:50:26 +0600

    Add files via upload

---
+++ Book/chapter 2/spaceDF.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=12345;
+    float b=4567.65;
+
+    printf("%5d\n", a);
+    printf("%6d\n", a);
+  //  printf("%3.4f\n", a);
+ //   printf("%2.5f\n", a);
+}
